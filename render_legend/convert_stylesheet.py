27d559a9b8d04b8bb01667a453dcaded0261bce9
Hartmut Holzgraefe <hartmut@php.net>
2020-05-04 10:04:53 +0200

    start by adding the stylesheet converter
    This turns out much simpler as in original RenderLegend as I plan to
    read data from a dummy PostGIS database instead of OSM XML directly.
    
    This is for two reasons:
    
    * Reading from the same datasource type makes rewriting the SQL
      statements in the layers unnecessary. Especially given that the
      original RenderLegend SQL "parser" is not able to parse some
      of the SQL statements in the current OpenStreetMap Carto style
      anyway, like e.g those using UNION, and as fully parsing SQL
      is hard, it's probably better to get rid of this approach
      completely.
    
    * The OSM input plugin is no longer part of core mapnik, and is
      usually missing from distribution packages. While the OGR
      input plugin can also read OSM XML, it behaves subtly different
      enough to make porting the original code hard.
    
    So we just replace the "dbname" to read from, and shift the conversion
    problem from "rewrite the layer SQL" to "move our generated OSM XML into
    a small PostGIS database", for which perfectly working tools already
    exist, or for which we could even generate SQL WKT gis data directly,
    still at a much lower complexity level than trying to build a general
    SQL parser.
    
    One item not covered yet: references to image files in the style file
    are usually relative to the files path. So for now we have to have
    original and rewritten style files in the same directory. Ideally
    such relative paths should be rewritten to become absolute paths
    referring to the original files, so that we can put the rewritten
    style whereever we want.

---
+++ render_legend/convert_stylesheet.py
@@ -0,0 +1,58 @@
+from lxml import etree
+
+def convert_stylesheet(old_file, new_file, old_db_name, new_db_name):
+    """ Convert a Mapnik XML stylesheet for legend creation use
+    * removes all Mapnik layers not using the "postgis" input plugin
+    * removes all "postgis" layers not referring to the "old_db_name"
+    * rewrites database name in remaining layers
+    TODO: convert relative file pathes into absolute ones
+    """
+
+    # parse original XML file
+    tree = etree.parse(old_file)
+    root = tree.getroot()
+
+    # parse global <Datasource> enntries that Layer datasources
+    # can refer to, replace "dbname" attribute right away
+    datasources = {}
+    for datasource in root.findall("./Datasource"):
+        datasource_name = datasource.get("name")
+        params = {}
+        for param in datasource.findall("./Parameter"):
+            pname = param.get("name")
+            ptext = param.text
+            if pname == 'dbname' and ptext == old_db_name:
+               param.text = new_db_name 
+            params[pname] = ptext
+        datasources[datasource_name] = params
+
+    # process all Layers
+    for layer in root.findall("./Layer"):
+        datasource = layer.find("./Datasource")
+
+        # if Datasource refers to a global one: take Parameter
+        # defaults from there
+        if datasource.get("base"):
+            params = datasources[datasource.get("base")]
+        else:
+            params = {}
+
+        # collect actual Datasource Parameters, overwriting
+        # parameters inherited form global Datasource
+        for param in datasource.findall("./Parameter"):
+            params[param.get("name")] = param.text
+
+        # remove unwanted layers, change "dbname" on those we keep
+        if params["type"] != "postgis":
+            root.remove(layer)
+        elif 'dbname' not in params:
+            root.remove(layer)
+        elif params["dbname"] != old_db_name:
+            root.remove(layer)
+        else:
+            dbname = datasource.find("./Parameter[@name='dbname']")
+            if dbname is not None:
+                dbname.text = new_db_name
+
+    # write back modified XML stylesheet
+    tree.write(new_file)
